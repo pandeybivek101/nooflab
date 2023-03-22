91edaaa80673e0f7679362aa07edeb613568b74e
Bivek Pandey <pandeyvivak25@hotmail.com>
2023-03-22 21:41:13 +0200

    database for storing the company information added

--- company/models.py
+++ company/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Company(models.Model):
+    """
+    businessId, name, registrationDate, companyForm are enough field to store information
+    detailsUri can be easily generated from the businessId
+    
+    """
+    businessId = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
+    registrationDate = models.DateField()
+    companyForm = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
--- company/views.py
+++ company/views.py
@@ -5,26 +5,30 @@ import requests
 
 # Create your views here.
 class FetchDataView(APIView):
+
+    def store(self):
+        pass
     
     def get(self, request):
 
-        postCodes = ["02100", "00140", "00930", "00710", "01730", "00500", "01760", "01690", "00510", "00180", "sjshshsh"]
+        postCodes = ["02100", "00140", "00930", "00710", "01730", "00500", "01760", "01690", "00510", "00180"]
 
         #looping through each postal address of Vantaa, Espoo and Helsinki
         for item in postCodes:
             try:
 
                 """
-                parameter to be pass along with the get requests as per the documentation
-                http://avoindata.prh.fi/bis/v1 two main parameters required for our case
-                maxResults to fetch maximum numbers of results and streetAddressPostCode to pass post code
+                parameter to be pass along with the get request as per the documentation of PRH.
+                endpoints http://avoindata.prh.fi/bis/v1 takes two parameters for our required scenario
+                maxResults to limit results and streetAddressPostCode to pass post code
                 """
 
                 parameters = {'maxResults': 20, 'streetAddressPostCode': item}
                 res = requests.get('http://avoindata.prh.fi/bis/v1', params=parameters)
-                print (res.text)
+                print (res.txt)
 
             except:
                 print(f"failed to fetch data from {item} postal address")
+
         return Response({'name': 'bivek'})
         
