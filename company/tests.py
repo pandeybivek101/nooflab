62423bf79d33be5101659aeb0e1d6eea80c47ee9
Bivek Pandey <pandeyvivak25@hotmail.com>
2023-03-23 21:22:29 +0200

    unit testing for GetDataView done

--- company/tests.py
+++ company/tests.py
@@ -1,53 +1,35 @@
 from django.test import TestCase
-from django.urls import reverse
-from rest_framework import status
 from rest_framework.test import APIClient
-from unittest.mock import patch, MagicMock
+from rest_framework import status
+from .models import Company
+from .serializers import CompanySerializer
 # Create your tests here.
 
 
-class FetchDataViewTestCase(TestCase):
+class GetDataViewTestCase(TestCase):
+    
     def setUp(self):
         self.client = APIClient()
 
-    @patch('requests.get')
-    def test_fetch_and_store_data(self, mock_get):
-        # Define mocked response from the remote API
-        mock_response = MagicMock()
-        mock_response.json.return_value = {
-            "results": [
-                {
-                    "businessId": "1234",
-                    "name": "Test Company",
-                    "registrationDate": "2022-01-01",
-                    "companyForm": "Ltd.",
-                    "postalcode": "02100"
-                }
-            ]
-        }
-        mock_response.status_code = status.HTTP_200_OK
+        # Create test companies with different postal codes
+        self.company1 = Company.objects.create(
+            businessId='55353-11', name='Test1', registrationDate='2022-04-01', companyForm='PY', postalcode='00011'
+        )
+        self.company2 = Company.objects.create(
+            businessId='4522-1', name='Test3', registrationDate='2021-02-01', companyForm='OX', postalcode='003331'
+        )
+        self.company3 = Company.objects.create(
+            businessId='8880-1', name='Test3', registrationDate='2025-2-01', companyForm='LTD', postalcode='00011'
+        )
 
-        # Patch requests.get to return the mocked response
-        mock_get.return_value = mock_response
 
-        # Make a GET request to the FetchDataView
-        url = reverse('fetch-data')
-        response = self.client.get(url)
-
-        # Ensure the response has a status code of 200
+    def test_get_data_view(self):
+        # Test retrieving data for a valid postal code
+        response = self.client.get('/postal_codes/00011/companies')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-        # Ensure the data was fetched and stored successfully
-        expected_messages = {
-            "02100": "Successfully fetched and stored",
-            "00140": "Successfully fetched and stored",
-            "00930": "Successfully fetched and stored",
-            "00710": "Successfully fetched and stored",
-            "01730": "Successfully fetched and stored",
-            "00500": "Successfully fetched and stored",
-            "01760": "Successfully fetched and stored",
-            "01690": "Successfully fetched and stored",
-            "00510": "Successfully fetched and stored",
-            "00180": "Successfully fetched and stored",
-        }
-        self.assertEqual(response.data['message'], expected_messages)
+        self.assertEqual(len(response.data), 2) # Expected 3 companies with postal code 00100
+        
+        # Test retrieving data for an invalid postal code
+        response = self.client.get('/postal_codes/6666666/companies')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 0) # Expected no companies with postal code 99999
